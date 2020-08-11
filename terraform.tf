9fdf869c997fe318384404ff4cfa0a5fbeb596a8
Alexander Mancevice <alexander.mancevice@hey.com>
2020-08-10 21:42:29 -0400

    terraform ~> 0.13

--- terraform.tf
+++ terraform.tf
@@ -4,11 +4,13 @@ terraform {
     key    = "terraform/api.tfstate"
     region = "us-east-1"
   }
+
+  required_version = "~> 0.13"
 }
 
 provider aws {
   region  = "us-east-1"
-  version = "~> 2.11"
+  version = "~> 3.1"
 }
 
 locals {
