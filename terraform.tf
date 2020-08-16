72b3e07bcee8abdba892483e14a2ee568d9e6506
Alexander Mancevice <alexander.mancevice@hey.com>
2020-08-16 08:56:29 -0400

    migrate to HTTP api

--- terraform.tf
+++ terraform.tf
@@ -20,6 +20,80 @@ locals {
   }
 }
 
+# HTTP API :: CORE
+
+resource aws_apigatewayv2_api http_api {
+  description   = "Brutalismbot slack API"
+  name          = "brutalismbot/slack"
+  protocol_type = "HTTP"
+  tags          = local.tags
+}
+
+resource aws_apigatewayv2_stage default {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  auto_deploy = true
+  description = "Brutalismbot HTTP API"
+  name        = "$default"
+  tags        = local.tags
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.http_api_logs.arn
+
+    format = jsonencode({
+      httpMethod     = "$context.httpMethod"
+      ip             = "$context.identity.sourceIp"
+      protocol       = "$context.protocol"
+      requestId      = "$context.requestId"
+      requestTime    = "$context.requestTime"
+      responseLength = "$context.responseLength"
+      routeKey       = "$context.routeKey"
+      status         = "$context.status"
+    })
+  }
+
+  lifecycle {
+    ignore_changes = [deployment_id]
+  }
+}
+
+resource aws_cloudwatch_log_group http_api_logs {
+  name              = "/aws/apigatewayv2/${aws_apigatewayv2_api.http_api.name}"
+  retention_in_days = 30
+  tags              = local.tags
+}
+
+# HTTP API :: MAPPING /slack
+
+resource aws_apigatewayv2_api_mapping slack {
+  api_mapping_key = "slack"
+  api_id          = aws_apigatewayv2_api.http_api.id
+  domain_name     = "api.brutalismbot.com"
+  stage           = aws_apigatewayv2_stage.default.id
+}
+
+# SLACKBOT V2
+
+module slackbot_v2 {
+  source  = "amancevice/slackbot/aws"
+  version = "19.4.0"
+
+  base_path                   = "/"
+  role_name                   = "brutalismbot-slack-lambda"
+  topic_name                  = "brutalismbot-slack"
+  lambda_function_name        = "brutalismbot-slack-http-api"
+  log_group_retention_in_days = 30
+
+  http_api_id            = aws_apigatewayv2_api.http_api.id
+  http_api_execution_arn = aws_apigatewayv2_api.http_api.execution_arn
+
+  lambda_kms_key_arn = data.aws_kms_alias.slackbot.target_key_arn
+  secret_name        = data.aws_secretsmanager_secret.slackbot.name
+
+  lambda_tags    = local.tags
+  log_group_tags = local.tags
+  role_tags      = local.tags
+}
+
 # SLACKBOT
 
 module slackbot {
@@ -50,31 +124,6 @@ data aws_acm_certificate cert {
 
 data aws_route53_zone website {
   name = "brutalismbot.com."
-}
-
-resource aws_api_gateway_base_path_mapping api {
-  api_id      = module.slackbot.api.id
-  base_path   = "slack"
-  domain_name = aws_api_gateway_domain_name.api.domain_name
-  stage_name  = module.slackbot.api_deployment.stage_name
-}
-
-resource aws_api_gateway_domain_name api {
-  certificate_arn = data.aws_acm_certificate.cert.arn
-  domain_name     = "api.brutalismbot.com"
-  security_policy = "TLS_1_2"
-}
-
-resource aws_route53_record api {
-  name    = aws_api_gateway_domain_name.api.domain_name
-  type    = "A"
-  zone_id = data.aws_route53_zone.website.id
-
-  alias {
-    evaluate_target_health = true
-    name                   = aws_api_gateway_domain_name.api.cloudfront_domain_name
-    zone_id                = aws_api_gateway_domain_name.api.cloudfront_zone_id
-  }
 }
 
 # SECRETS
